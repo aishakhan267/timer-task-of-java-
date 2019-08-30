97d2a4237c733db2c422df8f860e1cf494a5063f
aishakhan267 <49521671+aishakhan267@users.noreply.github.com>
2019-08-31 03:54:28 +0500

    added new

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
-let check;
-let isTimeEntered = false;
-function updateTime() {
-    if(isTimeEntered === true){
-        console.log(document.querySelector("span1").innerHTML = document.querySelector("#enter hour").value);
-    }
-   if(seconds === 59){
-       seconds = 0;
-       if(minutes === 59){
-           minutes = 0;
-           hours++;
-       }else{
-           minutes++;
-       }
-   }else{
-       seconds++
-   }
-   document.querySelector("#hours").innerHTML = hours;
-   document.querySelector("#minutes").innerHTML = minutes;
-   document.querySelector("#seconds").innerHTML = seconds;
-}
+let startTimer;
+let startTime = true;
+let bell = new Audio("beep.mp3");
 
 function start() {
- check = setInterval(updateTime, 1000);
+    if (startTime == true) {
+        document.querySelector("#hours").innerHTML = document.querySelector("#hourInput").value;
+
+        if (document.querySelector("#minInput").value > 60) {
+            alert("sorry");
+        } else {
+            document.querySelector("#minutes").innerHTML = document.querySelector("#minInput").value;
+        }
+
+        if (document.querySelector("#secInput").value > 60) {
+            alert("sorry");
+        } else {
+            document.querySelector("#seconds").innerHTML = document.querySelector("#secInput").value;
+        }
+
+        startTimer = setInterval(uptadeTime, 1000);
+        start = false;
+    }
 }
-function stop(){
-    clearInterval(check);
+
+function uptadeTime() {
+
+    if (document.querySelector("#seconds").innerHTML == 0 && document.querySelector("#minutes").innerHTML == 0 && document.querySelector("#hours").innerHTML == 0) {
+        bell.play();
+        document.querySelector("#minutes").innerHTML = 0;
+        document.querySelector("#seconds").innerHTML = 0;
+        document.querySelector("#hours").innerHTML = 0;
+
+    } else {
+        if (document.querySelector("#seconds").innerHTML == 0) {
+
+            document.querySelector("#seconds").innerHTML = 59;
+
+            if (document.querySelector("#minutes").innerHTML == 0) {
+
+                document.querySelector("#hours").innerHTML--;
+
+                document.querySelector("#minutes").innerHTML = 59;
+
+            } else {
+
+                document.querySelector("#minutes").innerHTML--;
+
+            }
+        } else {
+            document.querySelector("#seconds").innerHTML--;
+
+        }
+    }
 }
-function reset(){
-     hours = 0;
-     minutes = 0;
-     seconds = 0;
-     stop();
+
+function stop() {
+    location.reload();
 }
+
